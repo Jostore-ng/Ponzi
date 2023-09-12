372007c0bb7f623c6ef7a8fe372c1a86849e5e73
Jostore-ng <142936775+Jostore-ng@users.noreply.github.com>
2023-09-12 12:24:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,38 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
-
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
-
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
-});
-
-
-
-
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
-
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
-
-
-
-
-
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
-searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
-})
-
-
-
-
-
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
-}
-
-
-window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
-})
-
-
-
-const switchMode = document.getElementById('switch-mode');
-
-switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
+// Function to set dark mode preference in local storage
+function setDarkModePreference(isDarkMode) {
+	localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+  }
+  
+  // Function to toggle dark mode
+  function toggleDarkMode() {
+	const switchMode = document.getElementById('switch-mode');
+	const isDarkMode = switchMode.checked;
+  
+	if (isDarkMode) {
+	  document.body.classList.add('dark');
 	} else {
-		document.body.classList.remove('dark');
+	  document.body.classList.remove('dark');
 	}
-})
+  
+	// Save the dark mode preference in local storage
+	setDarkModePreference(isDarkMode);
+  }
+  
+  // Function to load dark mode preference from local storage
+  function loadDarkModePreference() {
+	const storedDarkMode = localStorage.getItem('darkMode');
+	const switchMode = document.getElementById('switch-mode');
+  
+	if (storedDarkMode === 'true') {
+	  switchMode.checked = true;
+	  toggleDarkMode();
+	}
+  }
+  
+  // Attach event listener to switch mode checkbox
+  const switchMode = document.getElementById('switch-mode');
+  switchMode.addEventListener('change', toggleDarkMode);
+  
+  // Load dark mode preference on page load
+  loadDarkModePreference();
+  
